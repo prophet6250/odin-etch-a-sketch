61389252bd378f16cfbe429a35034e230be229f6
Kumar Tatsat <rvk8878@gmail.com>
2022-02-25 13:33:07 +0530

    create square grid based on random parameter

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const createMatrix = (size = 16) => {
+  
+  const mainDiv = document.querySelector('.main');
+  
+  for (let i = parseInt(0); i < parseInt(size); i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = parseInt(0); j < parseInt(size); j++) {
+      let col = document.createElement('div');
+      col.classList.add('col');
+      row.appendChild(col);
+    }
+    
+    mainDiv.appendChild(row);
+  }
+
+}
+
+createMatrix();
